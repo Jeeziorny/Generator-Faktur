1e29f445559746947e0b9d33f7b6c6b63e11b5ea
matik98 <matik98@users.noreply.github.com>
2019-05-14 22:48:17 +0200

    Backend to share, print and safe invoice

--- app/src/main/java/com/example/generatorfaktur/MainActivity.kt
+++ app/src/main/java/com/example/generatorfaktur/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.generatorfaktur
 
+import android.Manifest
 import android.arch.persistence.room.Room
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
 import android.util.Log
 import android.view.View
 import com.beardedhen.androidbootstrap.TypefaceProvider
@@ -21,6 +24,21 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         TypefaceProvider.registerDefaultIconSets()
         supportActionBar!!.hide()
+
+        //APLICATION ASK FOR PERMISSION TO WORK WITH FILES IN EXTERNAS
+        //TODO rozwiazanie w razie gdy uzytkownik sie nie zgodzi
+        val permission = ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            val REQUEST_EXTERNAL_STORAGE = 1
+            val PERMISSIONS_STORAGE =
+                arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            ActivityCompat.requestPermissions(
+                this,
+                PERMISSIONS_STORAGE,
+                REQUEST_EXTERNAL_STORAGE
+            )
+        }
     }
 
     fun customerOnClick(view: View) {
