24708c264701d65676c8bb765866beeb81b7717d
matik98 <matik98@users.noreply.github.com>
2019-05-14 22:51:55 +0200

    Backend to share, print and safe invoice

---
+++ app/src/main/java/com/example/generatorfaktur/InvoiceUpload/PdfWriter.kt
@@ -0,0 +1,75 @@
+package com.example.generatorfaktur.InvoiceUpload
+
+import android.content.Context
+import android.content.res.AssetManager
+import android.os.Environment
+import android.print.PdfPrint
+import android.print.PrintAttributes
+import android.webkit.WebView
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
+
+import android.content.Intent
+import android.net.Uri
+import android.support.v4.content.FileProvider
+import com.example.generatorfaktur.R
+
+
+class PdfWriter (val context: Context, val fileName: String, val webView: WebView) {
+
+
+
+    fun writeAsTemporaryFile() {
+        context.deleteFile(fileName)
+        createWebPrintJob(webView)
+    }
+
+
+    fun uploadFile() {
+        val file = File(context.filesDir, fileName)
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.type = "application/pdf"
+
+        val uri = FileProvider.getUriForFile(context,
+            context.getString(R.string.file_provider_authority), file)
+        intent.putExtra(Intent.EXTRA_STREAM, uri)
+        context.startActivity(Intent.createChooser(intent, "Share PDF file"))
+
+
+    }
+
+    //COPY FILE FROM ASSETS TO EXTERNAL
+    fun copyToExternal(name: String) {
+        val input = FileInputStream(File(context.filesDir, fileName))
+        val output = FileOutputStream(makeOutputFile(name))
+        input.copyTo(output)
+    }
+
+    //CREATE FILE IN DCIM/InvoicePDF FOLDER
+    private fun makeOutputFile(fileName: String) : File {
+        val path =  Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM.plus("/InvoicePDF/"))
+        if (!path.exists()) {
+            path.mkdirs()
+        }
+        val file = File(path, fileName.plus(".PDF"))
+        file.createNewFile()
+
+        return file
+    }
+
+    //SAVES TO PDF WHAT WEBVIEW SHOWS
+    //THAT IS WHY MUST BE USED WEBVIEW FROM ACTIVITY
+    private fun createWebPrintJob(webView: WebView) {
+        val jobName = "Invoice Document"
+        val attributes = PrintAttributes.Builder()
+            .setMediaSize(PrintAttributes.MediaSize.ISO_A4)
+            .setResolution(PrintAttributes.Resolution("pdf", "pdf", 600, 600))
+            .setMinMargins(PrintAttributes.Margins.NO_MARGINS)
+            .build()
+        val pdfPrint = PdfPrint(context, attributes)
+        //val path = File(URL)
+        pdfPrint.write(webView.createPrintDocumentAdapter(jobName), fileName)
+    }
+
+}
---
+++ app/src/main/java/com/example/generatorfaktur/InvoiceUpload/PrintingManager.kt
@@ -0,0 +1,54 @@
+package com.example.generatorfaktur.InvoiceUpload
+
+import android.content.ContentValues.TAG
+import android.content.Context
+import android.print.PrintAttributes
+import android.print.PrintManager
+import android.util.Log
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
+class PrintingManager (private val context: Context){
+    private val webView: WebView
+
+    init {
+        //Create a WebView object for printing
+        webView = WebView(context)
+        webView.webViewClient = object : WebViewClient() {
+
+            override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?) = false
+
+            //RUNS WHEN NEW PAGE IS OPENED
+            override fun onPageFinished(view: WebView?, url: String?) {
+                Log.i(TAG, "page finished loading $url")
+                createWebPrintJob(view!!)
+            }
+        }
+    }
+
+    fun doWebViewPrint(url: String) {
+        webView.loadUrl(url)
+    }
+
+    private fun createWebPrintJob(webView: WebView) {
+
+        //Get a PrintManager instance
+        (context.getSystemService(Context.PRINT_SERVICE) as? PrintManager).let { printManager ->
+
+            //Name of printing document
+            val jobName = "Faktura Document"
+
+            //Get a print adapter instance
+            val printAdapter = webView.createPrintDocumentAdapter (jobName)
+
+            //Create a print job with name and adapter instance
+            printManager?.print(
+                jobName,
+                printAdapter,
+                PrintAttributes.Builder().build()
+            )
+
+        }
+    }
+}
